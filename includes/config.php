886826d776846f885fb8b6bd265aefc3a2651560
juniorferxd <69002764+juniorferxd@users.noreply.github.com>
2020-12-28 08:44:14 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -16,5 +16,5 @@
   define( 'DB_USER', 'uaveo8ukkstfpop7' );             // Set database user
   define( 'DB_PASS', '5HeG2pYv2zNmSxYcRBdM' );             // Set database password
   define( 'DB_NAME', 'bqof7tfigb5u0axgkqfq' );        // Set database name
-
+CC_FS_BUCKET=/some/empty/folder:bucket-569c770d-48d7-4957-9699-68d694eb3355-fsbucket.services.clever-cloud.com;
 ?>
