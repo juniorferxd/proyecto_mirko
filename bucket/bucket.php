97cf976055193b6f41c83206ee0c6be5d381f560
juniorferxd <69002764+juniorferxd@users.noreply.github.com>
2020-12-28 09:06:21 -0500

    Update bucket.php

--- bucket/bucket.php
+++ bucket/bucket.php
@@ -1,3 +1,3 @@
 <?php
-$CC_FS_BUCKET=/proyecto_mirko/bucket:bucket-569c770d-48d7-4957-9699-68d694eb3355-fsbucket.services.clever-cloud.com;
+$CC_FS_BUCKET=/bucket:bucket-569c770d-48d7-4957-9699-68d694eb3355-fsbucket.services.clever-cloud.com;
 ?>
